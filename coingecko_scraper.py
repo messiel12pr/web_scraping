636130a509286248e675562360bb49d988a170d1
Joel M. Gonzalez <joelm12pr@gmail.com>
2023-07-18 20:45:07 -0400

    refactored code

--- coingecko_scraper.py
+++ coingecko_scraper.py
@@ -11,28 +11,45 @@ class Coin_info:
     value: float
     date: str
 
-coin = defaultdict(list)
+def initialize_soup(url, header):
+    soup = None
 
-coins = ['Bitcoin', 'Ethereum', 'Tether', 'Cardano', 'Solana']
-URL = "https://www.coingecko.com"
-HEADERS = ({"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"})
+    try:
+        html = requests.get(url, headers=header).text
+        soup = BeautifulSoup(html, 'lxml')
 
-try:
-    html = requests.get(URL, headers=HEADERS).text
-    soup = BeautifulSoup(html, 'lxml')
+    except Exception as e:
+        print('ERROR: Could not initialize soup object')
+        print(e)
 
-except Exception as e:
-    print(e)
+    return soup
 
-# Getting coin info (name, value, and date)
-section = soup.find_all('tr')
-for i in range(len(section)):
-    now = datetime.now()
-    current_date = now.strftime("%Y-%m-%d")
-    coin_name = section[i].find('span', class_='lg:tw-flex font-bold tw-items-center tw-justify-between')
-    coin_value = section[i].find('span', class_='no-wrap')
+def get_coin_info(soup, coins):
+    coin = defaultdict(list)
+    section = soup.find_all('tr')
 
-    if(coin_name and coin_value and (coin_name.text.strip() in coins)):
-        # Parsing and storing coin info
-        parsed_value = float(coin_value.text.replace('$', '').replace(',', '').strip())
-        coin[coin_name.text.strip()].append(Coin_info(parsed_value, current_date))
+    for i in range(len(section)):
+        now = datetime.now()
+        current_date = now.strftime("%Y-%m-%d")
+        coin_name = section[i].find('span', class_='lg:tw-flex font-bold tw-items-center tw-justify-between')
+        coin_value = section[i].find('span', class_='no-wrap')
+
+        if(coin_name and coin_value and (coin_name.text.strip() in coins)):
+            # Parsing and storing coin info
+            parsed_value = float(coin_value.text.replace('$', '').replace(',', '').strip())
+            coin[coin_name.text.strip()].append(Coin_info(parsed_value, current_date))
+
+    return coin
+
+def main():
+    url = "https://www.coingecko.com"
+    header = ({"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}) 
+
+    coins = ['Bitcoin', 'Ethereum', 'Tether', 'Cardano', 'Solana']
+
+    soup = initialize_soup(url, header)
+    coin = get_coin_info(soup, coins)
+
+    print(coin)
+
+main()
